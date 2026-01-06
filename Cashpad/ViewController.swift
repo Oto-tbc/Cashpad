2a781087cb9ff59bdab0f657d2780688eec79770
Oto <sharvashidze0712@gmail.com>
2026-01-06 06:59:13 +0400

    First commit to greatness

---
+++ Cashpad/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Cashpad
+//
+//  Created by Oto Sharvashidze on 06.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
